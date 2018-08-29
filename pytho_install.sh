4e6b0d7416e5e8621a4034e8d19dc6282f8481b8
sa1987 <aswathi.tcs@gmail.com>
2018-08-29 08:47:47 +0530

    Update pytho_install.sh

--- pytho_install.sh
+++ pytho_install.sh
@@ -4,5 +4,7 @@ yum -y install yum-utils
 yum -y groupinstall development
 yum -y install https://centos7.iuscommunity.org/ius-release.rpm
 yum makecache
-yum -y install python36u
+yum -y install python34u  python34u-pip
 python3.6 -v
+echo "alias python=/usr/bin/python3.4" >> ~/.bash_profile
+source ~/.bash_profile
