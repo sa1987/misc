ead41f29cff32e7a0a10649e37e30abb8e0aea95
sa1987 <aswathi.tcs@gmail.com>
2018-08-29 08:37:05 +0530

    Update pytho_install.sh

--- pytho_install.sh
+++ pytho_install.sh
@@ -3,5 +3,6 @@ yum update -y
 yum -y install yum-utils
 yum -y groupinstall development
 yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+yum makecache
 yum -y install python36u
 python3.6 -v
